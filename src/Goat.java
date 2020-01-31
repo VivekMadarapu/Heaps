126a4362dd4e6b5f8c34b348d47ddb1eb3664167
Vivek Madarapu <vivek.madarapu.800@k12.friscoisd.org>
2020-01-31 16:30:37 -0600

    New Commit: Finished Goat

--- src/Goat.java
+++ src/Goat.java
@@ -21,22 +21,11 @@ class Goat {
                 cycleSizes.add(goat.size());
             }
             int daysNotEaten = 0;
-            while(goats.size() != 0 && daysNotEaten <= Collections.max(cycleSizes)){
-                boolean leave = true;
-                for (int j = 0; j < goats.size(); j++) {
-                    for (int k = 0; k < goats.size(); k++) {
-                        if(!Arrays.equals(goats.get(j).toArray(), goats.get(k).toArray())){
-                            leave = false;
-                        }
-                    }
-                }
-                if(leave){
-                    break;
-                }
+            while(goats.size() != 0 && daysNotEaten <= lcm(cycleSizes.toArray())){
                 List<Integer> milk = new ArrayList<>();
-                for (int j = 0; j < goats.size(); j++) {
-                    milk.add(goats.get(j).removeFirst());
-            }
+                for (Deque<Integer> goat : goats) {
+                    milk.add(goat.removeFirst());
+                }
                 List<Integer> milkSorted = new ArrayList<>(milk);
                 Collections.sort(milkSorted);
                 if(goats.size() == 1){
@@ -45,7 +34,7 @@ class Goat {
                     milk.clear();
                     daysNotEaten = 0;
                 }
-                else if(milkSorted.get(0) == milkSorted.get(1)){
+                else if(milkSorted.get(0).equals(milkSorted.get(1))){
                     for (int j = 0; j < goats.size(); j++) {
                         goats.get(j).addLast(milk.get(j));
                     }
@@ -62,7 +51,48 @@ class Goat {
                 }
                 days++;
             }
-            System.out.println(goats.size() + " " + (days));
+            System.out.println(goats.size() + " " + (days-daysNotEaten));
         }
     }
+
+    public static long lcm(Object[] element_array)
+    {
+        long lcm_of_array_elements = 1;
+        int divisor = 2;
+
+        while (true) {
+            int counter = 0;
+            boolean divisible = false;
+
+            for (int i = 0; i < element_array.length; i++) {
+
+                if ((int)element_array[i] == 0) {
+                    return 0;
+                }
+                else if ((int)element_array[i] < 0) {
+                    element_array[i] = (int)element_array[i] * (-1);
+                }
+                if ((int)element_array[i] == 1) {
+                    counter++;
+                }
+
+                if ((int)element_array[i] % divisor == 0) {
+                    divisible = true;
+                    element_array[i] = (int)element_array[i] / divisor;
+                }
+            }
+
+            if (divisible) {
+                lcm_of_array_elements = lcm_of_array_elements * divisor;
+            }
+            else {
+                divisor++;
+            }
+
+            if (counter == element_array.length) {
+                return lcm_of_array_elements;
+            }
+        }
+    }
+
 }
--- src/Heap.java
+++ src/Heap.java
@@ -1,58 +1,149 @@
-import java.util.ArrayList;
-import java.util.List;
-
+// Java implementation of Min Heap 
 public class Heap {
+    public int[] Heap;
+    public int size;
+    private int maxsize;
 
-    List<Integer> tree;
+    private static final int FRONT = 1;
 
-    public Heap(int root){
-        tree  = new ArrayList<>();
-        tree.add(null);
-        tree.add(root);
+    public Heap(int maxsize)
+    {
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
     }
 
-//    public void add(int val){
-//            tree[heapSize - 1] = val;
-//            siftUp(heapSize - 1);
-//    }
-
-//    private void siftUp(int index) {
-//        int parentIndex, tmp;
-//        if (index != 0) {
-//
-//            parentIndex = getParentIndex(index);
-//
-//            if (tree.get(parentIndex) > tree.get(index)) {
-//
-//                tmp = tree.get(parentIndex);
-//
-//                tree.set(parentIndex, tree.get(index));
-//
-//                tree.set(index, tmp);
-//
-//                siftUp(parentIndex);
-//            }
-//
-//        }
-//    }
-
-//    private int getParentIndex(int node) {
-//
-//    }
-
-    public void remove(){
-
+    // Function to return the position of 
+    // the parent for the node currently 
+    // at pos 
+    private int parent(int pos)
+    {
+        return pos / 2;
     }
 
-    public int peek(){
-        return tree.get(1);
+    // Function to return the position of the 
+    // left child for the node currently at pos 
+    private int leftChild(int pos)
+    {
+        return (2 * pos);
     }
 
-    public boolean isEmpty(){
-        return tree.size() == 1;
+    // Function to return the position of 
+    // the right child for the node currently 
+    // at pos 
+    private int rightChild(int pos)
+    {
+        return (2 * pos) + 1;
     }
 
-    public static void main(String[] args) {
-
+    // Function that returns true if the passed 
+    // node is a leaf node 
+    private boolean isLeaf(int pos)
+    {
+        if (pos >= (size / 2) && pos <= size) {
+            return true;
+        }
+        return false;
     }
-}
+
+    // Function to swap two nodes of the heap 
+    private void swap(int fpos, int spos)
+    {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    // Function to heapify the node at pos 
+    private void minHeapify(int pos)
+    {
+        // If the node is a non-leaf node and greater
+        // than any of its child 
+        if (!isLeaf(pos)) {
+            if (Heap[pos] > Heap[leftChild(pos)]
+                    || Heap[pos] > Heap[rightChild(pos)]) {
+
+                // Swap with the left child and heapify 
+                // the left child 
+                if (Heap[leftChild(pos)] < Heap[rightChild(pos)]) {
+                    swap(pos, leftChild(pos));
+                    minHeapify(leftChild(pos));
+                }
+
+                // Swap with the right child and heapify 
+                // the right child 
+                else {
+                    swap(pos, rightChild(pos));
+                    minHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    // Function to insert a node into the heap 
+    public void insert(int element)
+    {
+        if (size >= maxsize) {
+            return;
+        }
+        Heap[++size] = element;
+        int current = size;
+
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    // Function to print the contents of the heap 
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++) {
+            System.out.print(" PARENT : " + Heap[i]
+                    + " LEFT CHILD : " + Heap[2 * i]
+                    + " RIGHT CHILD :" + Heap[2 * i + 1]);
+            System.out.println();
+        }
+    }
+
+    // Function to build the min heap using 
+    // the minHeapify 
+    public void minHeap()
+    {
+        for (int pos = (size / 2); pos >= 1; pos--) {
+            minHeapify(pos);
+        }
+    }
+
+    // Function to remove and return the minimum 
+    // element from the heap 
+    public int remove()
+    {
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+        return popped;
+    }
+
+    // Driver code 
+    public static void main(String[] arg)
+    {
+        System.out.println("The Min Heap is ");
+        Heap minHeap = new Heap(15);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+        minHeap.minHeap();
+
+        minHeap.print();
+        System.out.println("The Min val is " + minHeap.remove());
+    }
+} 
